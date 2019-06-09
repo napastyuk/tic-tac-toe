c1a1cb69a98ae06c050f185ed605f4ea068216d0
inapastuk <inapastuk@beget.ru>
2019-06-09 23:48:42 +0300

    debug recursive function

--- task-1/index.js
+++ task-1/index.js
@@ -1,5 +1,27 @@
-function binarySearch(array, item) {
-  return array.indexOf(item); //placeholder
+function binarySearch(array, target, startPosition = 0 , endPosition = array.length-1) {
+  // array исходный массив для поиска
+  // target элемент, который надо найти
+  // startPosition, endPosition позиции в массиве для текущей иттерации поиска
+  console.log("----");
+  console.log("target " + target);
+  console.log("startPosition " + startPosition);
+  console.log("endPosition " + endPosition);
+
+  let middle = Math.floor(array.length/2);
+
+  if (array[middle] == target) return middle; //целевое значение ровно посередине, возращаем позицию
+  if (endPosition-1 === startPosition) {
+    console.log("fin");
+    //диапазон уменьшился до 2-х значений, поэтому проверяем оба
+    array[startPosition]==target ? startPosition : endPosition;
+  }
+  if (target > array[middle]) {
+    //целевое значение больше чем значение из середины, значит откидываем меньшую половину
+    return binarySearch(array, target, middle, endPosition)
+  } else {
+    //целевое значение меньше чем значение из середины, значит откидываем большую половину
+    return binarySearch(array, target, startPosition, middle)
+  }
 }
 
 //добавляем CommonJS экспорт что бы вызывать тесты напрямую
--- task-1/test/test.js
+++ task-1/test/test.js
@@ -3,7 +3,7 @@ var assert = require("chai").assert;
 
 describe("binarySearch", function() {
   it("binarySearch", function() {
-    assert.equal(index.binarySearch([1,2,3],2), 1);
+    assert.equal(index.binarySearch([0,1,2,3,4],1), 1);
   });
 
 });
