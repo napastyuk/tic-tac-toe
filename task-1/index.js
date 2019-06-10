c787ecd051dacac2d0f13d8aed4a356532a0b7d8
inapastuk <inapastuk@beget.ru>
2019-06-10 22:38:41 +0300

    clean code

--- task-1/index.js
+++ task-1/index.js
@@ -9,41 +9,29 @@ function binarySearch(array, target, startPosition = 0, endPosition = array.leng
   // - весь исходный массив/array упорядочен по возрастанию
   // - исходный элемент/target есть в массиве только в одном экземпляре. Если исходить из того, 
   //   что массив отсортирован, но в нём есть повторяющиеся числа, то возможны непрерывные 
-  //   последовательности одинаковых чисел типа 0,1,1,1,1,2. 
-  //   В случае если мы ищем число 1 алгоритм тогда найдет не первое вхождение числа.  
+  //   последовательности одинаковых чисел типа [0,1,1,1,1,2]. 
+  //   В случае, если мы ищем число "1" алгоритм найдет не первое вхождение числа.
   // 
 
-  // console.log("----");
-  // console.log("array " + array);
-  // console.log("target " + target);
-  // console.log("startPosition " + startPosition);
-  // console.log("endPosition " + endPosition);
-
-  if(startPosition < 0 || endPosition < 0) {
-    // console.log('startPosition и endPosition должны быть положительными числами')
+  if (startPosition < 0 || endPosition < 0) {
     return -1
   }
 
-  let middlePosition = Math.floor((startPosition + endPosition)/2);
-  // console.log("middlePosition " + middlePosition)
-  // console.log("----");
+  let middlePosition = Math.floor((startPosition + endPosition) / 2);
 
   if (endPosition === startPosition) {
-    // console.log("массив длиной в один элемент");
     //целевой  массив длиной всего один элемент, проверяем его
     return array[startPosition] === target ? startPosition : -1;
   }
 
   if (array[middlePosition] === target) {
-    // console.log("осталось 1 значение");
     //целевое значение ровно посередине, возращаем позицию
-    return middlePosition; 
+    return middlePosition;
   }
 
   if (endPosition - 1 === startPosition) {
-    // console.log("осталось 2 значения");
     //диапазон уменьшился до 2-х значений, поэтому проверяем оба
-    switch(target) {
+    switch (target) {
       case array[startPosition]: return startPosition
       case array[endPosition]: return endPosition
       default: return -1
@@ -51,15 +39,12 @@ function binarySearch(array, target, startPosition = 0, endPosition = array.leng
   }
 
   if (target > array[middlePosition]) {
-    // console.log("откидываем меньшую половину");
     //целевое значение больше чем значение из середины, значит откидываем меньшую половину
     return binarySearch(array, target, middlePosition, endPosition)
   } else {
-    // console.log("откидываем большую половину");
     //целевое значение меньше чем значение из середины, значит откидываем большую половину
     return binarySearch(array, target, startPosition, middlePosition)
   }
-
 }
 
 //добавляем CommonJS экспорт что бы вызывать тесты напрямую
--- task-1/test/test.js
+++ task-1/test/test.js
@@ -1,12 +1,12 @@
 var index = require("../index");
 var assert = require("chai").assert;
 
-function realNumbersArrayGenerator(n,isReal=true,maxRandNum=100) {
+function realNumbersArrayGenerator(n, isReal = true, maxRandNum = 100) {
   //генерируем сортированный массив размером n 
   let testArray = [];
-  for(i=1;i<=n;i++) {
+  for (i = 1; i <= n; i++) {
     let randomNumber;
-    isReal ? randomNumber = Math.random() : randomNumber = Math.floor(Math.random()*maxRandNum);
+    isReal ? randomNumber = Math.random() : randomNumber = Math.floor(Math.random() * maxRandNum);
     testArray.push(randomNumber);
   };
   testArray.sort(
@@ -17,69 +17,73 @@ function realNumbersArrayGenerator(n,isReal=true,maxRandNum=100) {
   return testArray;
 }
 
-describe("binarySearch", function() {
-  it("пустой массив", function() {
-    assert.equal(index.binarySearch([],1), -1);
+describe("binarySearch проверки размера массива", function () {
+  it("пустой массив", function () {
+    assert.equal(index.binarySearch([], 1), -1);
   });
-  it("массив из одного элемента присутвующий элемент", function() {
-    assert.equal(index.binarySearch([2],2), 0);
+  it("массив из одного элемента присутвующий элемент", function () {
+    assert.equal(index.binarySearch([2], 2), 0);
   });
-  it("массив из одного элемента отсутствующий элемент", function() {
-    assert.equal(index.binarySearch([0],1), -1);
+  it("массив из одного элемента отсутствующий элемент", function () {
+    assert.equal(index.binarySearch([0], 1), -1);
   });
-  it("массив из двух элементов присутвующий элемент", function() {
-    assert.equal(index.binarySearch([3,4],4), 1);
+  it("массив из двух элементов присутвующий элемент", function () {
+    assert.equal(index.binarySearch([3, 4], 4), 1);
   });
-  it("массив из двух элементов отсутствующий элемент", function() {
-    assert.equal(index.binarySearch([3,4],5), -1);
+  it("массив из двух элементов отсутствующий элемент", function () {
+    assert.equal(index.binarySearch([3, 4], 5), -1);
   });
-  it("массив из 3-х элементов присутвующий элемент", function() {
-    assert.equal(index.binarySearch([0,1,2],1), 1);
+  it("массив из 3-х элементов присутвующий элемент", function () {
+    assert.equal(index.binarySearch([0, 1, 2], 1), 1);
   });
-  it("массив из 3-х элементов отсутвующий элемент", function() {
-    assert.equal(index.binarySearch([0,1,2],3), -1);
+  it("массив из 3-х элементов отсутвующий элемент", function () {
+    assert.equal(index.binarySearch([0, 1, 2], 3), -1);
   });
-  it("ищем отсутвующее слишком большое значение", function() {
-    assert.equal(index.binarySearch([0,1,2,3,4,5,6,7,8,9],10), -1);
+});
+
+describe("binarySearch проверки отсутвующих элементов", function () {
+  it("ищем отсутвующее слишком большое значение", function () {
+    assert.equal(index.binarySearch([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], 10), -1);
   });
-  it("ищем отсутвующее слишком маленькое значение", function() {
-    assert.equal(index.binarySearch([2,3,4,5,6],1), -1);
+  it("ищем отсутвующее слишком маленькое значение", function () {
+    assert.equal(index.binarySearch([2, 3, 4, 5, 6], 1), -1);
   });
-  it("ищем отсутвующее значение co значениеми посередине существующих в массиве", function() {
-    assert.equal(index.binarySearch([2,4,6,8,10],5), -1);
+  it("ищем отсутвующее значение co значениеми посередине существующих в массиве", function () {
+    assert.equal(index.binarySearch([2, 4, 6, 8, 10], 5), -1);
   });
-  it("ищем первое значение в четном целочисленном массиве", function() {
-    let testArray = realNumbersArrayGenerator(10,false);
-    assert.equal(index.binarySearch(testArray,testArray[0]), 0);
+});
+
+describe("binarySearch проверки крайних элементов в сгенерированных массивах", function () {
+  it("ищем первое значение в четном целочисленном массиве", function () {
+    let testArray = realNumbersArrayGenerator(10, false);
+    assert.equal(index.binarySearch(testArray, testArray[0]), 0);
   });
-  it("ищем первое значение в нечетном целочисленном массиве", function() {
-    let testArray = realNumbersArrayGenerator(11,false);
-    assert.equal(index.binarySearch(testArray,testArray[0]), 0);
+  it("ищем первое значение в нечетном целочисленном массиве", function () {
+    let testArray = realNumbersArrayGenerator(11, false);
+    assert.equal(index.binarySearch(testArray, testArray[0]), 0);
   });
-  it("ищем последнее значение в четном целочисленном массиве", function() {
-    let testArray = realNumbersArrayGenerator(10,false);
-    assert.equal(index.binarySearch(testArray,testArray[9]), 9);
+  it("ищем последнее значение в четном целочисленном массиве", function () {
+    let testArray = realNumbersArrayGenerator(10, false);
+    assert.equal(index.binarySearch(testArray, testArray[9]), 9);
   });
-  it("ищем последнее значение в нечетном целочисленном массиве", function() {
-    let testArray = realNumbersArrayGenerator(11,false);
-    assert.equal(index.binarySearch(testArray,testArray[10]), 10);
+  it("ищем последнее значение в нечетном целочисленном массиве", function () {
+    let testArray = realNumbersArrayGenerator(11, false);
+    assert.equal(index.binarySearch(testArray, testArray[10]), 10);
   });
-  it("ищем первое значение в четном вещественном массиве", function() {
-    let testArray = realNumbersArrayGenerator(10,true);
-    assert.equal(index.binarySearch(testArray, testArray[0] ), 0);
+  it("ищем первое значение в четном вещественном массиве", function () {
+    let testArray = realNumbersArrayGenerator(10, true);
+    assert.equal(index.binarySearch(testArray, testArray[0]), 0);
   });
-  it("ищем первое значение в нечетном вещественном массиве", function() {
-    let testArray = realNumbersArrayGenerator(11,true);
-    assert.equal(index.binarySearch(testArray, testArray[0] ), 0);
-  });  
-  it("ищем последнее значение в четном вещественном массиве", function() {
-    let testArray = realNumbersArrayGenerator(10,true);
-    assert.equal(index.binarySearch(testArray, testArray[9] ), 9);
+  it("ищем первое значение в нечетном вещественном массиве", function () {
+    let testArray = realNumbersArrayGenerator(11, true);
+    assert.equal(index.binarySearch(testArray, testArray[0]), 0);
   });
-  it("ищем последнее значение в нечетном вещественном массиве", function() {
-    let testArray = realNumbersArrayGenerator(11,true);
-    assert.equal(index.binarySearch(testArray, testArray[10] ), 10);
+  it("ищем последнее значение в четном вещественном массиве", function () {
+    let testArray = realNumbersArrayGenerator(10, true);
+    assert.equal(index.binarySearch(testArray, testArray[9]), 9);
   });
-  //to do проверить на очень большом массиве
-  //to do проверить на очень больших числах в массиве
+  it("ищем последнее значение в нечетном вещественном массиве", function () {
+    let testArray = realNumbersArrayGenerator(11, true);
+    assert.equal(index.binarySearch(testArray, testArray[10]), 10);
+  });
 });
