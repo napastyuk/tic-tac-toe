39533502eb3355caf72e8e6005c4205b0de64979
ilya <napastyuk@live.ru>
2019-06-13 00:17:55 +0300

    add click liseners and figure showing

--- task-2/scripts.js
+++ task-2/scripts.js
@@ -7,6 +7,13 @@
         rootElementId: "ticTacToe"
     };
 
+    var playModel = {
+        field: [[],
+                [],
+                []],
+        currentFigureIsZero:true
+    }
+
     function init() {
         var targetElement = document.getElementById(config.rootElementId);
         if (!targetElement) {
@@ -24,20 +31,38 @@
             for (y = 1; y <= config.horizontalCellCount; y++) {
                 let tdEl = document.createElement('td');
                 trEl.appendChild(tdEl);
-                tdEl.appendChild(generateImgInCell(config.pathToZeroImg));                
-                tdEl.appendChild(generateImgInCell(config.pathToCrossImg));                
+                tdEl.dataset.cord = x+","+y
+                tdEl.appendChild(generateImgInCell(config.pathToZeroImg, "zero"));                
+                tdEl.appendChild(generateImgInCell(config.pathToCrossImg, "cross"));
+                tdEl.addEventListener('click', showFigure);
             }
             tbodyEl.appendChild(trEl);
         }
         rootEl.appendChild(tableEl);
     }
 
-    function generateImgInCell(pathToImg) {
+    function generateImgInCell(pathToImg,type) {
         let addingEl = document.createElement('object');
         addingEl.setAttribute("type", "image/svg+xml");
         addingEl.setAttribute("data", pathToImg);
+        addingEl.setAttribute("class", type);
         addingEl.hidden = true;
         return addingEl;
+    }
+
+    function showFigure(e) {
+        let target = e.target;
+        var elForShow;
+        let clickPosition = target.dataset.cord.split(','); //массив с позицией клика
+
+        target.removeEventListener('click', showFigure);        
+        if (playModel.currentFigureIsZero) {
+            elForShow = target.querySelector(".zero");
+        } else {
+            elForShow = target.querySelector(".cross");
+        }
+        elForShow.hidden = false;
+        playModel.currentFigureIsZero ? playModel.currentFigureIsZero = false : playModel.currentFigureIsZero = true;
     }
 
     init();
