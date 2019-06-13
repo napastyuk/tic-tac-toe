c3dcd7926af0bc41258d9bb53fdb1a1226a7a44f
ilya <napastyuk@live.ru>
2019-06-13 17:28:54 +0300

    debugg winner checkers

--- task-2/scripts.js
+++ task-2/scripts.js
@@ -5,7 +5,7 @@
     var config = {
         horizontalCellCount: 2, //количество ячеек на поле по горизонтали(отсчёт с нуля)
         verticalCellCount: 2, //тоже по вертикале
-        winCellCount:3, //количество ячеек заполненое подряд для выигрыша
+        winCellCount: 3, //количество ячеек заполненое подряд для выигрыша(отсчёт с нуля)
         pathToZeroImg: "img/zero.svg", //путь до картинки с ноликом
         pathToCrossImg: "img/cross.svg", //путь до картинки с крестиком
         rootElementId: "ticTacToe", //id тега контейнера в котором будет сгенерировано поле
@@ -19,13 +19,14 @@
         //"0" - в ячейке нолик
         //"1" - в ячейке крестик 
         field: [],
-        currentFigure: config.firstPlayerFigure //кто ходит прямо сейчас крестики ("1") или нолики ("0")
+        currentFigure: config.firstPlayerFigure, //кто ходит прямо сейчас крестики ("1") или нолики ("0")
+        winnerFigure: null
     }
 
     //часть отвечающая за View: генерация игрового поля
 
     function init() {
-        var targetElement = document.getElementById(config.rootElementId);
+        let targetElement = document.getElementById(config.rootElementId);
         if (!targetElement) {
             console.log("элемент с id ticTacToe не найден");
             return
@@ -36,10 +37,10 @@
     function generateGameField(rootEl) {
         let tableEl = document.createElement('table');
         let tbodyEl = document.createElement('tbody'); tableEl.appendChild(tbodyEl);
-        for (x = 0; x <= config.verticalCellCount; x++) {
+        for (let x = 0; x <= config.verticalCellCount; x++) {
             let trEl = document.createElement('tr');
             playModel.field[x] = []; //паралельно с генерацией элемента для DOM заполняем модель 
-            for (y = 0; y <= config.horizontalCellCount; y++) {
+            for (let y = 0; y <= config.horizontalCellCount; y++) {
                 let tdEl = document.createElement('td');
                 trEl.appendChild(tdEl);
                 tdEl.dataset.cord = x + "," + y;
@@ -66,7 +67,7 @@
 
     function showFigure(e) {
         let target = e.target;
-        var elForShow;
+        let elForShow;
         let clickPosition = target.dataset.cord.split(','); //массив с позицией клика        
         checkForWinner(clickPosition[0], clickPosition[1]);
         target.removeEventListener('click', showFigure);
@@ -83,58 +84,78 @@
     function checkForWinner(xPos, yPos) {
         playModel.field[xPos][yPos] = playModel.currentFigure; //обновляем модель
 
-        // по текущей позиции надо проверить горизонталь, вертикаль и диагональ
+        // по текущей позиции надо проверить горизонталь, вертикаль и диагональ в playModel.field
         function checkHorizontal() {
-            var sequenceLength = 0;
-            for (i = 0; i <= config.horizontalCellCount; i++) {
-                if (playModel.field[+xPos][i] == playModel.currentFigure) sequenceLength++;
+            let sequenceLength = 0;
+            for (let i = 0; i <= config.horizontalCellCount; i++) {
+                if (playModel.field[+xPos][i] == playModel.currentFigure) {
+                    sequenceLength++;
+                    if (sequenceLength == config.winCellCount) return sequenceLength;
+                } else sequenceLength = 0;
             };
             return sequenceLength;
         };
 
         function checkVertical() {
-            var sequenceLength = 0;
-            for (i = 0; i <= config.horizontalCellCount; i++) {
-                if (playModel.field[i][+yPos] == playModel.currentFigure) sequenceLength++;
+            let sequenceLength = 0;
+            for (let i = 0; i <= config.horizontalCellCount; i++) {
+                if (playModel.field[i][+yPos] == playModel.currentFigure) {
+                    sequenceLength++;
+                    if (sequenceLength == config.winCellCount) return sequenceLength;
+                } else sequenceLength = 0;
             };
             return sequenceLength;
         };
 
         function checkFirstDiagonal() {
-            var sequenceLength = 0;
-            for (i = 0; i <= config.horizontalCellCount; i++) {
-                if (playModel.field[i][i] == playModel.currentFigure) sequenceLength++;
+            let sequenceLength = 0;
+            for (let i = 0; i <= config.horizontalCellCount; i++) {
+                if (playModel.field[i][i] == playModel.currentFigure) {
+                    sequenceLength++;
+                    if (sequenceLength == config.winCellCount) return sequenceLength;
+                } else sequenceLength = 0;
             };
             return sequenceLength;
         };
 
         function checkSecondDiagonal() {
-            var sequenceLength = 0;
-            for (i = 0; i <= config.horizontalCellCount; ) {
-                for (z = config.verticalCellCount; z >= 0; ) {
-                    // console.log("[i][z] " + i + " " + z);
-                    if (playModel.field[i][z] == playModel.currentFigure)  {
-                        sequenceLength++;
-                        console.log("нашел в " + i + " " + z);
-                    i++;
-                    z--;    
-                    }
-                };                
-            };
-            console.log("sequenceLength " + sequenceLength);
-            return sequenceLength;
+            let sequenceLength = 0;
+            let z = config.winCellCount; //второй индекс для перебора
+            // for (let i = 0; i <= config.horizontalCellCount; i++) {
+            //     if (playModel.field[i][z] == playModel.currentFigure) {                    
+            //         sequenceLength++;
+            //         if (sequenceLength == config.winCellCount) return sequenceLength;
+            //     } else {
+            //         sequenceLength = 0; 
+            //     }
+            //     z--;
+            //     if (z==0) return;
+            // };
+            if (playModel.field[0][2] == playModel.currentFigure &&
+                playModel.field[1][1] == playModel.currentFigure &&
+                playModel.field[2][0] == playModel.currentFigure)
+                return config.winCellCount;
         };
 
-        console.log("checkHorizontal() " + checkHorizontal());
-        console.log("checkVertical() " + checkVertical());
-        console.log("checkFirstDiagonal() " + checkFirstDiagonal());
-        console.log("checkSecondDiagonal() " + checkSecondDiagonal());
+        if (checkHorizontal() == config.winCellCount ||
+            checkVertical() == config.winCellCount ||
+            checkFirstDiagonal() == config.winCellCount ||
+            checkSecondDiagonal() == config.winCellCount) {
+            playModel.winnerFigure = playModel.currentFigure;
+            showResult();
+        }
+    };
 
-        // if (checkHorizontal() == config.winCellCount || 
-        //     checkVertical() == config.winCellCount ||
-        //     checkFirstDiagonal() == config.winCellCount ||
-        //     checkSecondDiagonal() == config.winCellCount )
-        // console.log(playModel.currentFigure + " выиграл!");     
+    function showResult() {
+        let messageEl = document.createElement('p');
+        messageEl.setAttribute('class', 'winMessage')
+        let winMessage;
+        if (playModel.winnerFigure === null) winMessage = 'Ничья!'
+        else if (playModel.winnerFigure === 1) winMessage = 'Крестики выиграли!'
+        else if (playModel.winnerFigure === 0) winMessage = 'Нолики выиграли!';
+        messageEl.innerHTML = winMessage;
+        let targetElement = document.getElementById(config.rootElementId);
+        targetElement.appendChild(messageEl);
     }
     init();
 })();
